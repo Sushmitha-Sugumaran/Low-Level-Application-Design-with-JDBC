8ee1949cd0efdd1710430099284bdb18e0681e3d
Sushmitha-sugumaran <132361230+Sushmitha-Sugumaran@users.noreply.github.com>
2024-09-25 17:20:17 +0530

    Add files via upload

---
+++ Movie Ticket Booking (BookMyShow ) App with JDBC/src/JDBC.java
@@ -0,0 +1,10 @@
+import java.sql.*;
+public class JDBC {
+    static String url = "jdbc:mysql://localhost:3306/BookMyShowDataBase";
+    static String userName = "root";
+    static String password = "Sushmitha@123";
+    Connection con;
+    public static Connection getConnection() throws Exception{
+        return DriverManager.getConnection(url,userName,password);
+    }
+}
