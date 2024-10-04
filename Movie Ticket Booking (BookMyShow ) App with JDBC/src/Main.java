ab6e25bf1256f01ba31467beae94a311d4097ac5
Sushmitha-sugumaran <132361230+Sushmitha-Sugumaran@users.noreply.github.com>
2024-10-04 16:57:24 +0530

    Delete Movie Ticket Booking (BookMyShow ) App with JDBC directory

--- Movie Ticket Booking (BookMyShow ) App with JDBC/src/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        BOOKMYSHOWManagament management = BOOKMYSHOWManagament.getmanagement();
-        management.receptionSection();
-    }
-}
